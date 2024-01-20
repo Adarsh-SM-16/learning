f29827cc6578ae9fb9c0957ee9b7e0d0d937e122
Adarsh-SM-16 <108519704+Adarsh-SM-16@users.noreply.github.com>
2024-01-20 12:18:53 +0000

    h

--- adarsh.py
+++ adarsh.py
@@ -3,3 +3,4 @@ import filecmp
 output = subprocess.check_output(["git", "fetch", "origin"], stderr=subprocess.STDOUT, cwd='/workspaces/learning')
 with open("/workspaces/learning/_sources.yml","ab") as f:  # append to file
     f.write(output)
+print(f)
