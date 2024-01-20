ee2dcca3a88aff5f69464afc71fc5d9895fef382
Adarsh-SM-16 <108519704+Adarsh-SM-16@users.noreply.github.com>
2024-01-20 12:14:42 +0000

    hello

--- adarsh.py
+++ adarsh.py
@@ -1,5 +1,5 @@
 import subprocess
 import filecmp
-output = subprocess.check_output(["git", "fetch", "origin"], stderr=subprocess.STDOUT, cwd='/workspaces/learning/_sources.yml')
+output = subprocess.check_output(["git", "fetch", "origin"], stderr=subprocess.STDOUT, cwd='/workspaces/learning')
 with open("/workspaces/learning","ab") as f:  # append to file
     f.write(output)
